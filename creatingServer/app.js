81247a27915272be9540f22b5da49095c32d95ea
MemonaKhan <suhail1975pk@yahoo.co.uk>
2021-01-09 12:16:05 +0500

    create server

---
+++ creatingServer/app.js
@@ -0,0 +1,10 @@
+
+// creating server and minimal api
+
+var http = require('http');
+
+http.createServer(function(req,res){
+    console.log('got request')
+res.write('Yes Working');
+res.end();
+}).listen(8000);

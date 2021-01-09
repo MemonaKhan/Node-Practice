0e9f03e72954579d0a0c4d275e4f84d08ab4fcdb
MemonaKhan <suhail1975pk@yahoo.co.uk>
2021-01-09 16:20:06 +0500

    read html file

---
+++ RunHtmlFile/app.js
@@ -0,0 +1,17 @@
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req,res){
+    fs.readFile('./index.html',function(err,data){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.writeHead(200,{'Content-Type':'text/html'});
+            res.write(data);
+            res.end();
+        }
+
+        
+    })
+}).listen(8000);
